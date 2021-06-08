92eb7ccf3b2af937f1b5f104a5284049233fb53a
Hernandes-Silva <lertanisslobr@gmail.com>
2021-06-08 10:45:58 -0300

    config

--- virtual_store/settings/base.py
+++ virtual_store/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 env = environ.Env()
-env.read_env(str(BASE_DIR / ".env"))
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
